d15de6831497511373f8678e15fbae671d24a306
giorgos-grekis <georgrekis.github@gmail.com>
2026-03-15 00:57:11 +0200

    agentic search tool

---
+++ langGraph_components.py
@@ -0,0 +1,88 @@
+from dotenv import load_dotenv
+_ = load_dotenv()
+
+
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated
+import operator
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from IPython.display import Image, display
+
+# Only get back two responses form the search API.
+tool = TavilySearch(max_results=2)
+print(type(tool))
+print(tool.name) # tavily_search    ------   tavily_search_results_json 
+
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+
+
+class Agent:
+    def __init__(self, model, tools, system=""):
+        self.system = system
+        graph = StateGraph(AgentState)
+        graph.add_node("llm", self.call_openai)
+        graph.add_node("action", self.take_action)
+        graph.add_conditional_edges(
+            "llm", 
+            self.exists_action,
+            {True: "action", False: END} # If the function returns True, we're going to the action node, if False, we're going to the end node
+        )
+        graph.add_edge("action", "llm")
+        graph.set_entry_point("llm")
+        self.graph = graph.compile()
+        self.tools = {t.name: t for t in tools}
+        self.model = model.bind_tools(tools) # available tools to call
+
+    def exists_action(self, state: AgentState):
+        result = state['messages'][-1]
+        return len(result.tool_calls) > 0
+
+    def call_openai(self, state: AgentState):
+        messages = state['messages']
+        if self.system:
+            messages = [SystemMessage(content=self.system)] + messages
+        message = self.model.invoke(messages)
+        return {'messages': [message]}
+
+    def take_action(self, state: AgentState):
+        tool_calls = state['messages'][-1].tool_calls
+        results = []
+        for t in tool_calls:
+            print(f"Calling: {t}")
+            if not t['name'] in self.tools:      # check for bad tool name from LLM
+                print("\n ....bad tool name....")
+                result = "bad tool name, retry"  # instruct LLM to retry if bad
+            else:
+                result = self.tools[t['name']].invoke(t['args'])
+            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+        print("Back to the model!")
+        return {'messages': results}
+    
+
+prompt = """You are a smart research assistant. Use the search engine to look up information. \
+You are allowed to make multiple calls (either together or in sequence). \
+Only look up information when you are sure of what you want. \
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+"""
+
+model = ChatOpenAI(model="gpt-4.1-nano")  #reduce inference cost
+abot = Agent(model, [tool], system=prompt)
+
+
+# messages = [HumanMessage(content="What is the weather in sf?")]
+# result = abot.graph.invoke({"messages": messages})
+#
+# print(result["messages"][-1])
+#
+# messages = [HumanMessage(content="What is the weather in SF and LA?")]
+# result = abot.graph.invoke({"messages": messages})
+
+try:
+    display(abot.graph.get_graph().draw_mermaid_png())
+except:
+    print("display failed")
+    Image(abot.graph.get_graph().draw_mermaid_png())
--- main.py
+++ main.py
@@ -1,88 +1,115 @@
+# libraries
 from dotenv import load_dotenv
+import os
+from tavily import TavilyClient
+import requests
+from bs4 import BeautifulSoup
+from ddgs import DDGS
+import re
+import json
+from pygments import highlight, lexers, formatters
+
+
+# load environment variables from .env file
 _ = load_dotenv()
 
-
-from langgraph.graph import StateGraph, END
-from typing import TypedDict, Annotated
-import operator
-from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
-from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
-from IPython.display import Image, display
-
-# Only get back two responses form the search API.
-tool = TavilySearch(max_results=2)
-print(type(tool))
-print(tool.name) # tavily_search    ------   tavily_search_results_json 
+# connect
+client = TavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
 
 
-class AgentState(TypedDict):
-    messages: Annotated[list[AnyMessage], operator.add]
+# run search
+result = client.search("What is in Nvidia's new Blackwell GPU?",
+                       include_answer=True)
+
+# print the answer
+print(result["answer"])
 
 
-class Agent:
-    def __init__(self, model, tools, system=""):
-        self.system = system
-        graph = StateGraph(AgentState)
-        graph.add_node("llm", self.call_openai)
-        graph.add_node("action", self.take_action)
-        graph.add_conditional_edges(
-            "llm", 
-            self.exists_action,
-            {True: "action", False: END} # If the function returns True, we're going to the action node, if False, we're going to the end node
-        )
-        graph.add_edge("action", "llm")
-        graph.set_entry_point("llm")
-        self.graph = graph.compile()
-        self.tools = {t.name: t for t in tools}
-        self.model = model.bind_tools(tools) # available tools to call
+# choose location (try to change to your own city!)
 
-    def exists_action(self, state: AgentState):
-        result = state['messages'][-1]
-        return len(result.tool_calls) > 0
+city = "Athens"
 
-    def call_openai(self, state: AgentState):
-        messages = state['messages']
-        if self.system:
-            messages = [SystemMessage(content=self.system)] + messages
-        message = self.model.invoke(messages)
-        return {'messages': [message]}
-
-    def take_action(self, state: AgentState):
-        tool_calls = state['messages'][-1].tool_calls
-        results = []
-        for t in tool_calls:
-            print(f"Calling: {t}")
-            if not t['name'] in self.tools:      # check for bad tool name from LLM
-                print("\n ....bad tool name....")
-                result = "bad tool name, retry"  # instruct LLM to retry if bad
-            else:
-                result = self.tools[t['name']].invoke(t['args'])
-            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
-        print("Back to the model!")
-        return {'messages': results}
-    
-
-prompt = """You are a smart research assistant. Use the search engine to look up information. \
-You are allowed to make multiple calls (either together or in sequence). \
-Only look up information when you are sure of what you want. \
-If you need to look up some information before asking a follow up question, you are allowed to do that!
+query = f"""
+    what is the current weather in {city}?
+    Should I travel there today?
+    "weather.com"
 """
 
-model = ChatOpenAI(model="gpt-4.1-nano")  #reduce inference cost
-abot = Agent(model, [tool], system=prompt)
+
+ddg = DDGS()
+
+def search(query, max_results=6):
+    try:
+        results = ddg.text(query, max_results=max_results)
+        return [i["href"] for i in results]
+    except Exception as e:
+        print(f"returning previous results due to exception reaching ddg.")
+        results = [ # cover case where DDG rate limits due to high deeplearning.ai volume
+            "https://weather.com/weather/today/l/USCA0987:1:US",
+            "https://weather.com/weather/hourbyhour/l/54f9d8baac32496f6b5497b4bf7a277c3e2e6cc5625de69680e6169e7e38e9a8",
+        ]
+        return results
+
+for i in search(query):
+    print(i)
 
 
-# messages = [HumanMessage(content="What is the weather in sf?")]
-# result = abot.graph.invoke({"messages": messages})
-#
-# print(result["messages"][-1])
-#
-# messages = [HumanMessage(content="What is the weather in SF and LA?")]
-# result = abot.graph.invoke({"messages": messages})
+def scrape_weather_info(url):
+    """Scrape content from the given URL"""
+    if not url:
+        return "Weather information could not be found."
 
-try:
-    display(abot.graph.get_graph().draw_mermaid_png())
-except:
-    print("display failed")
-    Image(abot.graph.get_graph().draw_mermaid_png())
+    # fetch data
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return "Failed to retrieve the webpage."
+
+    # parse result
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+
+
+# use DuckDuckGo to find websites and take the first result
+url = search(query)[0]
+
+# scrape first wesbsite
+soup = scrape_weather_info(url)
+
+print(f"Website: {url}\n\n")
+print(str(soup.body)[:50000]) # limit long outputs
+
+# extract text
+weather_data = []
+for tag in soup.find_all(['h1', 'h2', 'h3', 'p']):
+    text = tag.get_text(strip=True)
+    weather_data.append(text)
+
+# combine all elements into a single string
+weather_data = "\n".join(weather_data)
+
+# remove all spaces from the combined text
+weather_data = re.sub(r'\s+', ' ', weather_data)
+
+print(f"Website: {url}\n\n")
+print(weather_data)
+
+
+# run search
+result = client.search(query, max_results=1)
+
+# print first result
+data = result["results"][0]["content"]
+
+print(data)
+
+# parse JSON
+parsed_json = json.loads(data.replace("'", '"'))
+
+# pretty print JSON with syntax highlighting
+formatted_json = json.dumps(parsed_json, indent=4)
+colorful_json = highlight(formatted_json,
+                          lexers.JsonLexer(),
+                          formatters.TerminalFormatter())
+
+print(colorful_json)
